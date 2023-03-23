7ace8f6f5552ef731551f5e125273ed1afe2bd6f
Amin Khodabande <whfk233@gmail.com>
2023-03-23 14:01:00 +0330

    typeorm is connected to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,27 @@ import { UsersModule } from './users/users.module';
 import { MessagesModule } from './messages/messages.module';
 import { MessageService } from './message/message.service';
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+          type: 'postgres',
+          host: config.get('DB_HOST'),
+          port: config.get('DB_PORT'),
+          username: config.get('DB_USERNAME'),
+          password: config.get('DB_PASSWORD'),
+          database: config.get('DB_NAME'),
+          entities: [],
+          synchronize: true
+      })
     }),
     UsersModule,
     MessagesModule
